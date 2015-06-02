816c9f50ba537ac8425112361433736516818f23
Jonathan Haenchen <jonathan.haenchen@flightaware.com>
2015-06-02 19:57:12 +0000

    Changes to cli.sh...
    ... to make it like, actually work and shit.

--- cli.sh
+++ cli.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Tool to automagically move VIM and Bash profile customizations into the appropriate places when git pulled. 
-# When you git clone/origin/whateverthefuck it creates a cli_tools folder, wherever you have done that. 
+# Tool to automagically move VIM (.vimrc) customizations into the appropriate places when git pulled. 
+# When you git clone it creates a cli_tools folder, wherever you have done that. 
 #
-# On FreeBSD systems, this is typically going to happen wherever I am set for ~, so I'll be using that. 
+# When you git pull, you can run this to overwrite the older .vimrc
+#
+# On BSD systems, this is typically going to happen wherever I am set for ~, so I'll be using that. 
 
-# move the .bash_profile into the appropriate place
-
-mv ~/cli_tools/.bash_profile ~/.bash_profile
 
 # move the .vimrc file into the appropriate place
 
-mv ~/cli_tools/.vim/.vimrc ~/.vim/.vimrc
+sudo cp ~/cli_tools/.vimrc ~/.vimrc
