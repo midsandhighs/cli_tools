115dc672036307d87989c36d9f9606d9b181c3df
midsandhighs <jonathan.haenchen@flightaware.com>
2015-07-02 21:16:30 +0000

    Maybe we can get this working sometime ever maybe
    Who the fuck knows.

---
+++ irc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#The beginning of a simple bash script to simplify my life re: joining IRC
+#
+#read output, if 1 thing, then, if next thing, then 
+#
+
+
+# Describe WTF is goin:q
+
